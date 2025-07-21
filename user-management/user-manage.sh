eb6e883a493e9e4d649bb7b489f0fc80c3d4bca2
Tanmay Bhurke <bhurketanmay@gmail.com>
2025-07-21 13:15:52 +0530

    21-July(user management changes)

--- user-management/user-manage.sh
+++ user-management/user-manage.sh
@@ -12,7 +12,7 @@ create_user(){
     read -rp "Enter shell for the user: " shell
     shell=${shell:-/bin/bash} #This is parameter expansion in bash
     # If the user does not provide any response then this will set to default /bin/bash
-    useradd -m -s ${shell} ${username} && echo "User created successfully" || echo "Failed to create a new user"
+    useradd -m -s "${shell}" "${username}" && echo "User created successfully" || echo "Failed to create a new user"
     # -m Tells the system to create the user's home directory (e.g., /home/username)
 }
 
@@ -24,7 +24,7 @@ modify_user(){
     # Here id $username will give all the relevant info for the user
     # uid=1001(someuser) gid=1001(someuser) groups=1001(someuser)
     # We are suppressing the stdout and stderr by redirecting to /dev/null
-    if id $username $>/dev/null; then
+    if id "$username" $>/dev/null; then
         echo "User exists!"
         echo "1. Change shell"
         echo "2. Rename user"
@@ -32,21 +32,39 @@ modify_user(){
         if [[ $ch == "1" ]]; then
            echo "Choice is to change the shell"
            read -rp "Enter the appropriate shell (/bin/sh, /bin/zsh): " shell
-           usermod -s ${shell} ${username} && echo "shell has been modified for the user" || echo "Failed to modify the shell for the user"
+           usermod -s "${shell}" "${username}" && echo "shell has been modified for the user" || echo "Failed to modify the shell for the user"
         elif [[ $ch == "2" ]]; then
            echo "Renaming the user"
            read -rp "Enter new username" new_user_name
-           usermod -l ${new_user_name} ${username}  && echo "Username has been renamed successfully"
+           usermod -l "${new_user_name}" "${username}"  && echo "Username has been renamed successfully"
         #    https://www.serverlab.ca/tutorials/linux/administration-linux/how-to-rename-linux-users-and-their-home-directory/
-           usermod -d /home/${new_user_name} -m username
+           usermod -d "/home/${new_user_name}" -m username
         #    This will create a new home directory for new_user_name and copy the contents from old homedir to new one
-           groupmod -n ${new_user_name} ${username}
+           groupmod -n "${new_user_name}" "${username}"
            echo "Renaming has been done"
         else
            echo "Invalid Modification choice"
         fi
     else
         echo "Username with that ID does not exists"
+    fi
+}
+
+delete_user(){
+    echo "User deletion script"
+    read -rp "Enter the user to be deleted: " username
+
+    if id "${username}" &>/dev/null; then
+        read -rp "Do you want to delete the home directory of the user as well? (y/n)" ans
+        if [[ "${ans}" == "y" ]]; then
+        # -r option is recursive, it deletes home directory, user info from /etc/passwd, /etc/shadow, /var/mail/username (mail spool file)
+            userdel -r "${username}" && echo "User and his home directory has been deleted!!" || echo "Failed to delete User and home directory"
+        else
+            userdel "${username}" && echo "User  has been deleted!!" || echo "Failed to delete user"
+
+        fi
+    else
+        echo "Invalid user. This user does not exist"
     fi
 }
 
@@ -70,7 +88,9 @@ case "${choice}" in
     echo "User Modification script ended"
     ;;
     3)
-        echo "Deleting user here"
+    echo "Deleting existing user here"
+    delete_user
+    echo "User deletion ended"
     ;;
     *)
         echo "default (none of above)"
